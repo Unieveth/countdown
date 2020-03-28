a83c8b821bbf304cfc72d1d06e49991c473e8ba5
Unieveth <connor0523@gmail.com>
2020-03-28 15:51:27 -0500

    2 New Timers!

---
+++ 1 Mins/script.js
@@ -0,0 +1,46 @@
+const progressBar = document.getElementsByClassName("progress-bar")[0];
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar);
+  const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+  progressBar.style.setProperty("--width", width + 0.1);
+}, 62);
+var Timer = function(opts) {
+  var self = this;
+
+  self.opts     = opts || {};
+  self.element  = opts.element || null;
+  self.minutes  = opts.minutes || 0;
+  self.seconds  = opts.seconds || 30;
+
+  self.start = function() {
+    self.interval = setInterval(countDown, 1000);
+  };
+
+  self.stop = function() {
+    clearInterval(self.interval);
+  };
+
+  function countDown() {
+    self.seconds--;
+    if (self.minutes == 0 && self.seconds == 0) {
+      self.stop();
+    }
+
+    if (self.seconds < 0) {
+      self.seconds = 59;
+      self.minutes--;
+    }
+
+    if (self.seconds <= 9) { self.seconds = '0' + self.seconds; }
+
+    self.element.textContent = self.minutes + ':' + self.seconds;
+  }
+};
+
+var myTimer = new Timer({
+  minutes: 0,
+  seconds: 60,
+  element: document.querySelector('#timer')
+});
+
+myTimer.start();
